72710fd61f0514fa1721d3d56b287f00ab8474fa
sonamsd <ssdhadiwal.it@gmail.com>
2021-04-15 22:54:58 -0500

    setting up aws-amplify

--- react-basic-components/src/App.js
+++ react-basic-components/src/App.js
@@ -3,6 +3,10 @@ import { Router } from 'react-router-dom';
 import history from './services/history';
 import Routes from './routes/index';
 
+import Amplify from 'aws-amplify'
+import config from './aws-exports'
+Amplify.configure(config)
+
 function App() {
   return (
       <Router history = {history}>
